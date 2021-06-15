0aaff0d72feeee0bf347c47d85fa0a72601e78cd
David Petter <cfdavidpetter@gmail.com>
2021-06-15 20:13:41 -0300

    update code exp

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+from src.main.configs import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ src/infra/entities/users.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from src.infra.config import Base
+
+
+class Users(Base):
+    """ Users Entity """
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    id_pet = relationship("Pets")
+
+    def __repr__(self):
+        return f"User [name={self.name}]"
+
+    def __eq__(self, other):
+        if (
+            self.id == other.id
+            and self.name == other.name
+            and self.password == other.password
+        ):
+            return True
+        return False
